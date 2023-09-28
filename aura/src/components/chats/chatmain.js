8f7c0d424acdabe6f3eb8b5ac7e86c7249a2ffc3
Suraj <surajkumar15081947@gmail.com>
2023-09-28 23:14:25 +0530

    adding Documentation

--- aura/src/components/chats/chatmain.js
+++ aura/src/components/chats/chatmain.js
@@ -3,14 +3,14 @@ import Msgdemo from "./msgdemo";
 import './chatmain.css'
 
 export default function Chatmain(){
-    let shiv = "OM Namah Shivaya OM Namo Narayana Om devi durgaya namaha Jai jai jai jai jaggannath horibolo hori hori bolo";
+    let shiv = "Hi!!";
     return(
         <div className="chat-main-new"> 
            <div className="chat-sec">
               <Msgdemo num="1" text={shiv}/>
-              <Msgdemo num="2" text="Har har mahadev hori bol jai maa durga"/>
-              <Msgdemo num="1" text="hello"/>
-              <Msgdemo num="2" text="Hi Suraj"/>
+              <Msgdemo num="2" text="Hi Beta"/>
+              <Msgdemo num="1" text="Kaise ho aap?"/>
+              <Msgdemo num="2" text="Badhiya.. Tum batao?"/>
            </div>
         </div>
     )
